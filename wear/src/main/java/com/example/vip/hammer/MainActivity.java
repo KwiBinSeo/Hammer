7dd9eb48c1971c247b8182b589a36e5b7c3c045a
CGLAB_VIP <eirtjrnlqls@gmail.com>
2017-07-28 00:00:57 +0900

    버튼이벤트 추가

--- wear/src/main/java/com/example/vip/hammer/MainActivity.java
+++ wear/src/main/java/com/example/vip/hammer/MainActivity.java
@@ -1,15 +1,10 @@
 package com.example.vip.hammer;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.wearable.activity.WearableActivity;
-import android.support.wearable.view.BoxInsetLayout;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
-
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.Locale;
 
 public class MainActivity extends WearableActivity {
 
@@ -21,7 +16,18 @@ public class MainActivity extends WearableActivity {
         setContentView(R.layout.activity_main);
         setAmbientEnabled();
 
-        
+        workBtn = (Button) findViewById(R.id.WorkBtn);
+        upBtn = (Button) findViewById(R.id.UpBtn);
+        downBtn = (Button) findViewById(R.id.DownBtn);
+
+        workBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, Main2Activity.class);
+                startActivity(intent); //
+                finish(); // 현재 스택 종료
+            }
+        });
     }
 
     @Override
